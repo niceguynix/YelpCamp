7b8c5bb6d2a16611641fc1e1fce47bbec02d7eb7
Manoj <its200manoj@gmail.com>
2022-01-26 16:22:11 +0530

    fix: fixed error function

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ const validateCampground = (req,res,next) =>{
     
     const {error} = campgroundSchema.validate(req.body);
     if (error){
-        const msg = error.details().map(el=>el.message).join(',')
+        const msg = error.details.map(el=>el.message).join(',')
         throw new ExpressError(msg ,400)
     }
     else{
@@ -51,7 +51,7 @@ const validateCampground = (req,res,next) =>{
 const validateReview = (req,res,next) =>{
     const {error} = reviewSchema.validate(req.body);
     if (error){
-        const msg = error.details().map(el=>el.message).join(',')
+        const msg = error.details.map(el=>el.message).join(',')
         throw new ExpressError(msg ,400)
     }
     else{
