7b7b96f5cffdcbbe7dc895ae1a743b0e79621bf0
Manoj <its200manoj@gmail.com>
2022-01-28 14:28:55 +0530

    refactor: seperate review routes

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const { campgroundSchema , reviewSchema } = require('./schemas');
 const morgan = require('morgan');
 
 const campgrounds = require('./routes/campgrounds');
+const reviews = require('./routes/reviews');
+
+
 //still using local database
 mongoose.connect('mongodb://localhost:27017/yelp-camp',{
     useNewUrlParser:true,
@@ -38,18 +41,9 @@ app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'))
 
 
-const validateReview = (req,res,next) =>{
-    const {error} = reviewSchema.validate(req.body);
-    if (error){
-        const msg = error.details.map(el=>el.message).join(',')
-        throw new ExpressError(msg ,400)
-    }
-    else{
-        next();
-    }
-}
 
 app.use('/campgrounds',campgrounds);
+app.use('/campgrounds/:id/reviews',reviews);
 
 app.get('/',(req,res)=>{
     res.render('home');
@@ -57,21 +51,7 @@ app.get('/',(req,res)=>{
 
 
 
-app.post('/campgrounds/:id/reviews',validateReview,catchAsync(async (req,res) => {
-    const campground= await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
 
-app.delete('/campgrounds/:id/reviews/:reviewId',catchAsync(async (req,res)=>{
-    const {id,  reviewId} = req.params;
-    await Campground.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});    
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/campgrounds/${id}`);
-}))
 
 app.all('*',(req,res,next)=>{
     next(new ExpressError('Page Not Found',404));
