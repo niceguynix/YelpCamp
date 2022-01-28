1792420e824645e4bbdad834c1a318f10d6d81a4
Manoj <its200manoj@gmail.com>
2022-01-28 14:45:29 +0530

    refactor: add static assets

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.set('views',path.join(__dirname,'/views'));
 app.use(morgan('tiny'));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'))
-
+app.use(express.static(path.join(__dirname,'/public')));
 
 
 app.use('/campgrounds',campgrounds);
