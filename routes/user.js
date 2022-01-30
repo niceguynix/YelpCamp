a7d8d11809ec57a2bebb24217560395402a5ff7b
Manoj <its200manoj@gmail.com>
2022-01-30 16:56:50 +0530

    feat: add register logic

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const user = require('../models/user');
 const router = express.Router();
 const User = require('../models/user');
+const catchAsync = require('../utils/catchAsync');
 
 router.get('/register',(req,res)=>{
     res.render('users/register');
 })
 
-router.post('/register',async (req,res)=>{
-    res.send(req.body);
-})
+router.post('/register',catchAsync( async (req,res)=>{
+    try{
+    const {username , email , password} =req.body;
+    const user = new User({username , email});  
+    const registeredUser = await User.register(user,password);
+    req.flash('success','Welcome to YelpCamp '+user.username);
+    res.redirect('/campgrounds');
+    }catch(err){
+        req.flash('error',err.message);
+        res.redirect('/register');
+    }
+}));
 
 module.exports = router;
