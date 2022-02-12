b841e1057e52dd4ccbb8c2780a29ba3c073a439f
Manoj <its200manoj@gmail.com>
2022-02-12 14:29:50 +0530

    map popout

--- public/js/showpagemap.js
+++ public/js/showpagemap.js
@@ -1,11 +1,17 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: campground.geometry.coordinates, // starting position [lng, lat]
-    zoom: 9 // starting zoom
+    container: 'map', 
+    style: 'mapbox://styles/mapbox/light-v10', 
+    center: campground.geometry.coordinates, 
+    zoom: 9 
 });
 
 new mapboxgl.Marker()
     .setLngLat([-74.5, 40])
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(
+                 `<h3>${campground.title}</h3><p>${campground.location}</p>`
+                )
+    )
     .addTo(map)
